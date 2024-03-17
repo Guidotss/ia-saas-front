b2b41a3fe57f6c7f8d291ea452489676637ea689
Guidotss <guidoolguin@hotmail.com>
2024-03-17 00:55:17 -0300

    Terminando vista de input para enviar mensajes

---
+++ src/components/chat/input/ChatInput.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useRef, useState } from "react";
+
+export const ChatInput = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    if (textAreaRef.current) {
+      // Establecer la altura del textarea según su contenido
+      textAreaRef.current.style.height = "10px";
+      textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+      textAreaRef.current.style.maxHeight = "90px";
+    }
+  }, [inputValue]);
+
+  useEffect(() => {
+    if (buttonRef.current && textAreaRef.current) {
+        buttonRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+        buttonRef.current.style.maxHeight = "90px";
+    }
+  })
+
+  return (
+    <div className="w-full flex justify-center">
+      <div className="absolute bottom-10">
+        <form
+          className="flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <textarea
+            className="2xl:w-[33rem] min-h-[20px] py-3 px-6 rounded-l-xl bg-slate-800 text-white outline-none resize-none overflow-auto"
+            placeholder="Type a message"
+            ref={textAreaRef}
+            value={inputValue}
+            rows={2}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+            <div className="flex items-center">
+                <button
+                ref={buttonRef}
+                type="submit"
+                className="bg-slate-800 text-white px-4 py-2 rounded-r-xl hover:bg-slate-700 transition-colors duration-300 ease-in-out"
+                >
+                Send
+                </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
